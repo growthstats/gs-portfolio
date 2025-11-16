683d7245f5cdfcf8b18b909ea412346ebde79b40
AtharvaCM <necrons999@gmail.com>
2025-11-16 14:38:43 +0530

    chore: add husky an pr template

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+	printWidth: 100,
+	trailingComma: 'all',
+	singleQuote: true,
+	semi: false,
+	plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
