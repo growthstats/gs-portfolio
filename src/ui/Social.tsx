c8b83a48952439665999b99eb480a027b0006bb2
AtharvaCM <necrons999@gmail.com>
2026-01-28 22:01:34 +0530

    feat: add threads social

--- src/ui/Social.tsx
+++ src/ui/Social.tsx
@@ -2,73 +2,73 @@ import { getSite } from '@/sanity/lib/queries'
 import CTA from './CTA'
 import { cn } from '@/lib/utils'
 import {
-	FaBluesky,
-	FaFacebookF,
-	FaGithub,
-	FaInstagram,
-	FaLinkedinIn,
-	FaTiktok,
-	FaXTwitter,
-	FaYoutube,
-	FaTelegram,
+  FaBluesky,
+  FaFacebookF,
+  FaGithub,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTiktok,
+  FaXTwitter,
+  FaYoutube,
+  FaTelegram,
+  FaThreads,
 } from 'react-icons/fa6'
 import { IoIosLink } from 'react-icons/io'
 import type { ComponentProps } from 'react'
 
 export default async function Social({ className }: ComponentProps<'div'>) {
-	const { social } = await getSite()
+  const { social } = await getSite()
 
-	if (!social?.items?.length) return null
+  if (!social?.items?.length) return null
 
-	return (
-		<nav className={cn('group flex flex-wrap items-center', className)}>
-			{social.items.map((item, key) => {
-				switch (item._type) {
-					case 'link':
-						return (
-							<CTA
-								className="px-2 py-1 group-has-[a:hover]:opacity-50 hover:!opacity-100"
-								link={item}
-								key={key}
-								style={"link"}
-							>
-								<Icon url={item.external} aria-label={item.label} />
-							</CTA>
-						)
+  return (
+    <nav className={cn('group flex flex-wrap items-center', className)}>
+      {social.items.map((item, key) => {
+        switch (item._type) {
+          case 'link':
+            return (
+              <CTA
+                className="px-2 py-1 group-has-[a:hover]:opacity-50 hover:!opacity-100"
+                link={item}
+                key={key}
+                style={'link'}
+              >
+                <Icon url={item.external} aria-label={item.label} />
+              </CTA>
+            )
 
-					default:
-						return null
-				}
-			})}
-		</nav>
-	)
+          default:
+            return null
+        }
+      })}
+    </nav>
+  )
 }
 
-function Icon({
-	url,
-	...props
-}: { url?: string } & React.ComponentProps<'svg'>) {
-	if (!url) return null
+function Icon({ url, ...props }: { url?: string } & React.ComponentProps<'svg'>) {
+  if (!url) return null
 
-	return url?.includes('t.me') ? (
-		<FaTelegram {...props} />
-	) : url?.includes('bsky.app') ? (
-		<FaBluesky {...props} />
-	) : url?.includes('facebook.com') ? (
-		<FaFacebookF {...props} />
-	) : url?.includes('github.com') ? (
-		<FaGithub {...props} />
-	) : url?.includes('instagram.com') ? (
-		<FaInstagram {...props} />
-	) : url?.includes('linkedin.com') ? (
-		<FaLinkedinIn {...props} />
-	) : url?.includes('tiktok.com') ? (
-		<FaTiktok {...props} />
-	) : url?.includes('twitter.com') || url?.includes('x.com') ? (
-		<FaXTwitter {...props} />
-	) : url?.includes('youtube.com') ? (
-		<FaYoutube {...props} />
-	) : (
-		<IoIosLink {...props} />
-	)
+  return url?.includes('t.me') ? (
+    <FaTelegram {...props} />
+  ) : url?.includes('bsky.app') ? (
+    <FaBluesky {...props} />
+  ) : url?.includes('facebook.com') ? (
+    <FaFacebookF {...props} />
+  ) : url?.includes('github.com') ? (
+    <FaGithub {...props} />
+  ) : url?.includes('threads.net') || url?.includes('threads.com') ? (
+    <FaThreads {...props} />
+  ) : url?.includes('instagram.com') ? (
+    <FaInstagram {...props} />
+  ) : url?.includes('linkedin.com') ? (
+    <FaLinkedinIn {...props} />
+  ) : url?.includes('tiktok.com') ? (
+    <FaTiktok {...props} />
+  ) : url?.includes('twitter.com') || url?.includes('x.com') ? (
+    <FaXTwitter {...props} />
+  ) : url?.includes('youtube.com') ? (
+    <FaYoutube {...props} />
+  ) : (
+    <IoIosLink {...props} />
+  )
 }
